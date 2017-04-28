6fac3297fcaf2f84117edbe97462fb1fed3c8b4d
Vinay <Vinay.bv@accionlabs.com>
2017-04-28 17:29:52 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,25 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+//routes
+import { RouteModule } from './routes/app.routes';
+import { routingComponents } from './routes/app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    routingComponents,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes/app.routes.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './../dashboard/dashboard.component';
+import { LoginComponent } from './../login/login.component';
+
+
+const routes : Routes = [
+     { path:"dashboard", component: DashboardComponent },
+     { path:"login", component: LoginComponent }
+];
+
+@NgModule({
+
+	imports: [
+         RouterModule.forRoot(routes)
+	],
+	exports:[
+         RouterModule
+	]
+})
+export class RouteModule {}
+export const routingComponents = [ DashboardComponent, LoginComponent ];
